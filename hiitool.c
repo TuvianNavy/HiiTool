6b38bbc1da093c29e4784939887fdba6949b931b
TuvianNavy <Tuviannavy@outlook.com>
2020-08-20 17:11:17 +0900

    definitions cleanups

--- hiitool.c
+++ hiitool.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/UefiBootServicesTableLib.h>
-#include <Library/UefiLib.h>
 #include <Library/UefiHiiServicesLib.h>
-#include <Library/HiiLib.h>
-#include <Guid/MdeModuleHii.h>
 
 int main(int argc, char **argv)
 {
   EFI_HII_DATABASE_PROTOCOL *HiiDatabase;
+  EFI_HII_CONFIG_ROUTING_PROTOCOL *HiiConfigRouting;
   EFI_STATUS efi_status;
   UINTN buflen;
   EFI_HII_HANDLE dummybufptr;
   EFI_HII_HANDLE *HiiHandleBuffer;
 
-  efi_status = gBS->LocateProtocol (
-               &gEfiHiiDatabaseProtocolGuid,
-               NULL,
-               (VOID **) &HiiDatabase);
-  if (EFI_ERROR (efi_status)) {
-    return 1;
-  }
+  efi_status = gBS->LocateProtocol (&gEfiHiiDatabaseProtocolGuid, NULL, (VOID **) &HiiDatabase);
+  if (EFI_ERROR (efi_status)) { return 1; }
+  efi_status = gBS->LocateProtocol (&gEfiHiiConfigRoutingProtocolGuid, NULL, (VOID **) &HiiConfigRouting);
+  if (EFI_ERROR (efi_status)) { return 1; }
+
   efi_status = HiiDatabase->ListPackageLists (
-               gHiiDatabase,
+               HiiDatabase,
                EFI_HII_PACKAGE_SIMPLE_FONTS,
                NULL,
                &buflen,
@@ -35,7 +31,7 @@ int main(int argc, char **argv)
     return 1;
   }
   efi_status = HiiDatabase->ListPackageLists (
-               gHiiDatabase,
+               HiiDatabase,
                EFI_HII_PACKAGE_SIMPLE_FONTS,
                NULL,
                &buflen,
@@ -45,5 +41,6 @@ int main(int argc, char **argv)
     return 1;
   }
   FreePool (HiiHandleBuffer);
+  printf("all OK\n");
   return 0;
 }
