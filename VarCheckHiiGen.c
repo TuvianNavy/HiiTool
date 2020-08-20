70c952f97adbaf54b90f23e1c78e3d8252b69aa6
TuvianNavy <Tuviannavy@outlook.com>
2020-08-21 03:02:09 +0900

    guidtostr (commented out)

--- VarCheckHiiGen.c
+++ VarCheckHiiGen.c
@@ -726,3 +726,20 @@ VarCheckParseHiiDatabase (
   }
 }
 
+/*
+CHAR8 *
+GuidToStr (
+  IN EFI_GUID *guid;
+  )
+{
+  CHAR8 guid_string[37];
+  snprintf (
+    guid_string, sizeof(guid_string),
+    "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+    guid->Data1, guid->Data2, guid->Data3,
+    guid->Data4[0], guid->Data4[1], guid->Data4[2],
+    guid->Data4[3], guid->Data4[4], guid->Data4[5],
+    guid->Data4[6], guid->Data4[7]);
+  return guid_string;
+}
+*/
